43db7f41f2cc0e91af0e06e67cf610b231984a68
Frederico Teixeira Azevedo <2020955533@teiacoltec.org>
2021-08-08 15:32:25 -0300

    Second commit
    Commit E06-Persistencia-Registrando último acesso

--- app/src/main/java/br/ufmg/coltec/tp/e06persistencia/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e06persistencia/MainActivity.java
@@ -2,6 +2,7 @@ package br.ufmg.coltec.tp.e06persistencia;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,18 +16,37 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1;
+    private static final String APP_PREF_ID = "SP_MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnTirarFoto = findViewById(R.id.btn_tirar_foto);
+        SharedPreferences pref2 = this.getSharedPreferences(APP_PREF_ID, 0);
+        String data = pref2.getString("chaveData", "");
 
+        if (data != null) {
+            Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+        }
+
+        String currentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+        String currentTime = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
+
+        SharedPreferences pref = this.getSharedPreferences(APP_PREF_ID, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("chaveData", currentDate + " - " + currentTime);
+        editor.commit();
+
+
+        Button btnTirarFoto = findViewById(R.id.btn_tirar_foto);
         btnTirarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
