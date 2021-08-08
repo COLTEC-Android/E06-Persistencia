b307b994bf31873d8bdb04f604d0e400897113a6
Frederico Teixeira Azevedo <2020955533@teiacoltec.org>
2021-08-08 13:50:59 -0300

    First commit
    Commit E06-Persistencia-Salvando Fotos no Aparelho

--- app/src/main/java/br/ufmg/coltec/tp/e06persistencia/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e06persistencia/MainActivity.java
@@ -1,13 +1,84 @@
 package br.ufmg.coltec.tp.e06persistencia;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTirarFoto = findViewById(R.id.btn_tirar_foto);
+
+        btnTirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        try {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+                final ImageView foto = findViewById(R.id.foto);
+                final Bitmap photo = (Bitmap) data.getExtras().get("data");
+                foto.setImageBitmap(photo);
+
+                Button btnSalvarFoto = findViewById(R.id.btn_salvar_foto);
+                btnSalvarFoto.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String filename = "MyPhoto.png";
+                        File file = new File(MainActivity.this.getExternalFilesDir(null), filename);
+
+                        if (Environment.getExternalStorageState(file).equals(Environment.MEDIA_MOUNTED)) {
+                            try {
+                                FileOutputStream out = new FileOutputStream(file);
+                                photo.compress(Bitmap.CompressFormat.PNG, 100, out);
+                                out.flush();
+                                out.close();
+                                Toast.makeText(MainActivity.this, R.string.string_foto_salva, Toast.LENGTH_LONG).show();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (Exception ex) {
+            Toast.makeText(MainActivity.this, ex.toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
