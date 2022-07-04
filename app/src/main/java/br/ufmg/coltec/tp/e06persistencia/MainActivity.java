457517c3ffa5bfdb2c0a4c41179e43159c596802
Samuel <samuel@gmail.com>
2022-07-04 19:32:53 -0300

    Android E06: Persistência - Registrando último acesso - finalizada

--- app/src/main/java/br/ufmg/coltec/tp/e06persistencia/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e06persistencia/MainActivity.java
@@ -2,10 +2,14 @@ package br.ufmg.coltec.tp.e06persistencia;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,16 +22,42 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
 
     private static int FOTO_CODE = 1;
     private ImageView imageView;
+    private String APP_PREF_ID = "PersistenciaID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd/MM/yyyy - HH:mm", Locale.getDefault());
+        String currentDateTime = currentDate.format(new Date());
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(APP_PREF_ID,0) ;
+        SharedPreferences.Editor editor = pref.edit();
+        String valueStr = pref.getString("data/hora","");
+
+        if(valueStr != ""){
+
+            Toast.makeText(MainActivity.this, "Ultimo acesso:"+valueStr, Toast.LENGTH_LONG).show();
+            editor.putString("data/hora",currentDateTime);
+            editor.commit();
+
+        }else{
+            Toast.makeText(MainActivity.this,"Primeiro acesso:"+ currentDateTime, Toast.LENGTH_LONG).show();
+            editor.putString("data/hora",currentDateTime);
+            editor.commit();
+        }
+
+
 
         Button fotoBtn=findViewById(R.id.btn_foto);
         Button salvarBtn = findViewById(R.id.btn_salvar);
